22e8b0fe1232cf52145a28086bf17a432adfc1a5
Arun Sharma <arun143420@gmail.com>
2025-03-21 13:27:31 +0530

    Added initial test cases and simple sum logic

---
+++ src/stringCalculator.js
@@ -0,0 +1,13 @@
+  /**
+   * A function to add any number of numbers in a given string.
+   * The function takes a string as an argument, extracts the numbers from it
+   * and returns the sum of the extracted numbers.
+   * @param {string} numbers - a string of comma or newline separated numbers
+   * @returns {number} - the sum of the extracted numbers
+   */
+export function add(numbers) {
+    if (!numbers) return 0;  
+    let delimiter = /,|\n/;
+    const numArray = numbers.split(delimiter).map(n => parseInt(n, 10));
+    return numArray.filter(n => n <= 1000).reduce((sum, num) => sum + num, 0);
+  }
